bc4c9a9ebef20e462bbc087da22be7c714254a76
BrunoEllerbach <bruno.ellerbach@zup.com.br>
2023-05-11 10:18:21 -0300

    Added binding and created new screen

---
+++ app/src/main/java/com/example/convidados/GuestFromActivity.kt
@@ -0,0 +1,31 @@
+package com.example.convidados
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import com.example.convidados.databinding.ActivityGuestFromBinding
+
+class GuestFromActivity : AppCompatActivity(), View.OnClickListener{
+
+    private lateinit var binding: ActivityGuestFromBinding
+    private lateinit var viewModel: GuestFormViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityGuestFromBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this).get(GuestFormViewModel::class.java)
+
+        binding.buttonSave.setOnClickListener(this)
+        binding.radioPresent.isChecked = true
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.button_save){
+
+        }
+    }
+}
