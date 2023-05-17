cb6989fe33bd603299a2185aa9856252b65d14f0
BrunoEllerbach <bruno.ellerbach@zup.com.br>
2023-05-17 11:44:08 -0300

    Delete items now implemented

--- app/src/main/java/com/example/convidados/viewmodel/AllGuestsViewModel.kt
+++ app/src/main/java/com/example/convidados/viewmodel/AllGuestsViewModel.kt
@@ -4,7 +4,6 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.example.convidados.model.GuestModel
 import com.example.convidados.repository.GuestRepository
 
@@ -13,6 +12,11 @@ class AllGuestsViewModel(application: Application) : AndroidViewModel(applicatio
     private val listAllGuests = MutableLiveData<List<GuestModel>>()
     val guests: LiveData<List<GuestModel>> = listAllGuests
     fun getAll() {
+        listAllGuests.value = listOf()
         listAllGuests.value = repository.getAll()
+    }
+
+    fun delete(id: Int) {
+        repository.delete(id)
     }
 }
